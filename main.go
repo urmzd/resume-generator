725d2ffe97d5207e7caa18882486e34fca5b90e4
Urmzd Mukhammadnaim <urmzd.consulting@gmail.com>
2026-02-07 20:07:36 -0600

    feat: add embedded templates and version information support

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
 	"github.com/urmzd/resume-generator/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.EmbeddedTemplatesFS = EmbeddedTemplates
+	cmd.Version = version
+	cmd.Commit = commit
+	cmd.BuildDate = date
 	cmd.Execute()
 }
